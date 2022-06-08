0dbb7334feca8b11312c49bdb68eb30c28c8d7aa
Jym Patel <jympatel@yahoo.com>
2022-06-08 05:37:19 +0000

    update name and code

---
+++ 01_hello-world/hello.py
@@ -0,0 +1,5 @@
+# code by @PATELPRATHAM007
+# program - print "hello, world!"
+
+# print function
+print("hello, world!") # print(arg1, arg2, arg3, ...)
--- program_01/hello.py
+++
@@ -1,5 +0,0 @@
-#this program made by pratham patel
-#program - type hello word
-
-#add print keyword
-print("hello word")
